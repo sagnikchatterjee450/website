6d7c399a207ce267aec4f2f72df35b68f13c4410
Anish <anishmandalme1234@outlook.com>
2022-09-29 22:27:18 +0530

    website updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+import logo from "./assets/logo.svg";
+import { instagramIcon, twitterIcon, linkedinIcon } from "./assets/socials";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="main" className="h-screen w-full bg-black font-grot text-white">
+      <div className="flex justify-center items-center flex-col h-full w-full">
+        <img src={logo} alt="Tezucka logo" />
+        <h1 className="font-black sm:text-8xl text-5xl pointer-events-none">
+          Coming Soon
+        </h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
 
-export default App
+      <div className="fixed bottom-0 left-0 h-1/6 flex justify-center items-center flex-col w-full">
+        <span className="font-semibold text-xl">Follow us</span>
+        <div className="flex">
+          <a
+            href="https://twitter.com/tezucka"
+            className="sm:mx-20 mx-10 my-5"
+            target="_blank"
+          >
+            <img src={twitterIcon} alt="Twitter logo" />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/tezucka-tech/"
+            className="sm:mx-20 mx-10 my-5"
+            target="_blank"
+          >
+            <img src={linkedinIcon} alt="Linkedin logo" />
+          </a>
+          <a
+            href="https://www.instagram.com/tezucka/"
+            className="sm:mx-20 mx-10 my-5"
+            target="_blank"
+          >
+            <img src={instagramIcon} alt="Instagram logo" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/assets/socials/index.ts
@@ -0,0 +1,3 @@
+export { default as instagramIcon } from "./instagram.svg";
+export { default as linkedinIcon } from "./linkedin.svg";
+export { default as twitterIcon } from "./twitter.svg";
