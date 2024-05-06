05f53cadf61e95fedfda7716153e9c1e222298c6
anish-mandal <anishmandalme1234@outlook.com>
2024-05-06 15:19:23 +0530

    add promo video

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import logo from "./assets/logo.svg";
-import { instagramIcon, twitterIcon, linkedinIcon } from "./assets/socials";
-
 export default function App() {
+  const logo = "/icons/logo.svg";
+  const socials = {
+    twitterIcon: "/icons/socials/twitter.svg",
+    linkedinIcon: "/icons/socials/linkedin.svg",
+    instagramIcon: "/icons/socials/instagram.svg",
+  };
+
   return (
     <div id="main" className="h-screen w-full bg-black font-grot text-white">
       <div className="flex justify-center items-center flex-col h-full w-full">
@@ -9,9 +13,20 @@ export default function App() {
         <h1 className="font-black sm:text-8xl text-5xl pointer-events-none">
           Coming Soon
         </h1>
+
+        <a href="#video" className="font-bold underline text-xl m-5">
+          Our promo video
+        </a>
       </div>
 
-      <div className="fixed bottom-0 left-0 h-1/6 flex justify-center items-center flex-col w-full">
+      <div
+        id="video"
+        className="bg-black flex justify-center items-center w-full h-screen"
+      >
+        <video src="/videos/tezucka-health.mp4" width="500" controls></video>
+      </div>
+
+      <div className="fixed bottom-0 left-0 h-1/6 flex justify-center items-center flex-col w-full bg-black">
         <span className="font-semibold text-xl">Follow us</span>
         <div className="flex">
           <a
@@ -19,21 +34,21 @@ export default function App() {
             className="sm:mx-20 mx-10 my-5"
             target="_blank"
           >
-            <img src={twitterIcon} alt="Twitter logo" />
+            <img src={socials.twitterIcon} alt="Twitter logo" />
           </a>
           <a
             href="https://www.linkedin.com/company/tezucka-tech/"
             className="sm:mx-20 mx-10 my-5"
             target="_blank"
           >
-            <img src={linkedinIcon} alt="Linkedin logo" />
+            <img src={socials.linkedinIcon} alt="Linkedin logo" />
           </a>
           <a
             href="https://www.instagram.com/tezucka/"
             className="sm:mx-20 mx-10 my-5"
             target="_blank"
           >
-            <img src={instagramIcon} alt="Instagram logo" />
+            <img src={socials.instagramIcon} alt="Instagram logo" />
           </a>
         </div>
       </div>
--- src/assets/socials/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as instagramIcon } from "./instagram.svg";
-export { default as linkedinIcon } from "./linkedin.svg";
-export { default as twitterIcon } from "./twitter.svg";
